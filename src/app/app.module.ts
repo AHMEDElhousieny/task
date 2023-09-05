bbca5a3ab8de09d59bbd3dd53e44184562526a8f
ahmed <ahmedelhousieny8@gmail.com>
2023-09-05 18:10:15 +0300

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { MatCardModule } from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {MatIconModule} from '@angular/material/icon';
+
+// import { AgmCoreModule } from '@agm/core';
+
+
+
+import { CarComponent } from './components/car/car.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatCardModule,
+    MatGridListModule,
+    MatIconModule,
+    // // AgmCoreModule.forRoot({
+    // //   apikey: ''
+    // })
   ],
   providers: [],
   bootstrap: [AppComponent]
